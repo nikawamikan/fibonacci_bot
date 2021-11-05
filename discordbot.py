e565e2123397bedfe117b2765563b3eb0b7348d3
nikawamikan <wkwktktkkrkr@outlook.jp>
2021-11-05 12:13:29 +0900

    new

--- discordbot.py
+++ discordbot.py
@@ -4,64 +4,85 @@ import fibomap
 import todo
 import yaml
 import bump
+import re
 
+# yamlから設定を取得
 with open('botconfig.yml') as file:
     conf = yaml.safe_load(file)
-    print(conf['help']['cin']['targetwd'])
 
+# yamlからオブジェクトを分割して使いやすくします
+
+# helpオブジェクト
+helpob = conf['help']
+
+# mapオブジェクト
 mapob = conf['map']
 map_list = joblib.load(mapob['file'])
 
+# todoオブジェクト
 todoob = conf['todo']
 Todo_list = joblib.load(todoob['file'])
 
+# channelidオブジェクト
+channelid = conf['channel_id']
+
+# botの隠し機能的なヤツ
+bot_response = conf['bot_response']
+
+pepper = conf['pepper']
+
 client = discord.Client()
+
+# Help用のembed関数 先頭文字列から targetwd を正規表現で検索します
+
+
+def help_embed_sender(message, obj: map):
+    if(re.match(obj['targetwd'], message.content) != None):
+        helpEmbed = discord.Embed(
+            title=obj['title'],
+            color=obj['color'],
+            description=obj['description'])
+        message.channel.send(embed=helpEmbed)
+
+# BOTがなんか読み取ってresponseします
+
+
+def bot_response(message, obj: map):
+    if(re.search(obj['targetwd'], message.content) != None):
+        file_img = discord.File(obj['img'])
+        message.channel.send(file=file_img)
 
 
 @client.event
 async def on_message(message):
 
-    await bump.bump(message=message)
-    await bump.dissoku(message=message)
+    #  ----------------------隠しコマンド---------------------------#
+    bot_response(message=message, obj=bot_response['yagarekudasai'])
+    bot_response(message=message, obj=bot_response['gomennasorry'])
+    bot_response(message=message, obj=bot_response['fibo'])
 
-    # ----------------------その他コマンド---------------------------#
-    if ("すみません" in message.content) or ("ごめん" in message.content):
-        file_img = discord.File("image/sorry.gif")
-        await message.channel.send(file=file_img)
-
-    if("やがれください" in message.content):
-        file_yagare = discord.File("image/simple_yagare.gif")
-        await message.channel.send(file=file_yagare)
-    if("ふぃぼなっち" in message.content):
-        file_img = discord.File("image/fibonacci.png")
-        await message.channel.send(file=file_img)
-
+# bumpとかdissokuとか検知する子
     if message.author.bot:
+        bump.bot_observer(message=message, pepper=pepper['disboard'])
+        bump.bot_observer(message=message, pepper=pepper['dissoku'])
+
         return
 
-    if (message.channel.id != conf['channel_id']['fibo_chat']) & (message.channel.id != conf['channel_id']['bot']):
+    if (message.channel.id != channelid['fibo_chat']) & (message.channel.id != channelid['bot']):
         return
 
     command = message.content.split()
 
     if "/test" in message.content:
         await message.channel.send(":gg::+1:")
-# ------------------------help-------------------------------#
-    if(conf['help']['help']['targetwd'] in message.content) or (conf['help']['help']['targetwd2'] in message.content):
-        helpEmbed = discord.Embed(
-            title=conf['help']['help']['title'],
-            color=conf['help']['help']['color'],
-            description=conf['help']['help']['description'])
-        await message.channel.send(embed=helpEmbed)
 
-    if(conf['help']['cin']['targetwd'] in message.content):
-        helpEmbed = discord.Embed(
-            title=conf['help']['cin']['title'],
-            color=conf['help']['cin']['color'],
-            description=conf['help']['cin']['description'])
-        await message.channel.send(embed=helpEmbed)
+# ------------------------help-------------------------------#
+
+    help_embed_sender(message, obj=helpob['cin'])
+    help_embed_sender(message, obj=helpob['help'])
 
 # -----------------------Todoリスト----------------------------#
+
     if("/todo" in message.content) or ("・とど" in message.content):
         await todo.get_todo(message, command, Todo_list, map)
 
