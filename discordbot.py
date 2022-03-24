2d8ce6dd0863cb410995204adb0da25a2f80bf2e
nikawamikan <wkwktktkkrkr@outlook.jp>
2022-03-24 19:34:25 +0900

    BotTokenの分離など

--- discordbot.py
+++ discordbot.py
@@ -1,33 +1,30 @@
 from todo import get_todo
 from config import todoob
 from config import botcom
-# from config import pepper
 from config import channelid
 from config import map_list
 from config import helpob
 from config import token
-
+import os
+from dotenv import load_dotenv
 
 import discord
-from discord.ext import tasks, commands
 import re
 import fibomap
-# import bump
 
 from botsender import if_bot_response
 from botsender import if_help_embed_sender
+
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+bot = discord.Bot()
+
 
 # 正規表現のプリコンパイル
 todopt = re.compile(todoob['targetwd'])
 
 client = discord.Client()
-
-bot = commands.Bot(command_prefix='/')
-
-
-@bot.command()
-async def testd(ctx, arg):
-    await ctx.send(arg)
 
 
 @client.event
@@ -37,10 +34,6 @@ async def on_message(message):
     await if_bot_response(message=message, obj=botcom['yagarekudasai'])
     await if_bot_response(message=message, obj=botcom['gomennasorry'])
     await if_bot_response(message=message, obj=botcom['fibo'])
-
-# bumpとかdissokuとか検知する子
-    # bump.bot_observer(message=message, pepper=pepper['disboard'])
-    # bump.bot_observer(message=message, pepper=pepper['dissoku'])
 
 # 通常のコマンド判定の為botかどうかなどを判定
     if message.author.bot or (message.channel.id != channelid['fibo_chat']) & (message.channel.id != channelid['bot']):
@@ -69,4 +62,4 @@ async def on_message(message):
         return
 
 
-client.run(token)
+client.run(TOKEN)
